65a2e2188a6a8a6cda5c01ece4fedb1c5ef45cf2
Alimzhan <kabylhanovalimzan@gmail.com>
2026-01-20 13:35:54 +0600

    Kabylkhnaov Alimzhan

---
+++ src/edu/aitu/oop3/entities/Project.java
@@ -0,0 +1,20 @@
+package edu.aitu.oop3.entities;
+
+public class Project {
+    private int id;
+    private String name;
+    private int userId;
+
+    public Project() {}
+    public Project(String name, int userId) {
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+}
---
+++ src/edu/aitu/oop3/entities/Task.java
@@ -0,0 +1,40 @@
+package edu.aitu.oop3.entities;
+
+import java.sql.Timestamp;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private Timestamp deadline;
+    private String status;
+    private int projectId;
+
+    public Task() {}
+
+    public Task(String title, String description, Timestamp deadline, int projectId) {
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.projectId = projectId;
+        this.status = "PENDING";
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public Timestamp getDeadline() { return deadline; }
+    public void setDeadline(Timestamp deadline) { this.deadline = deadline; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+    public int getProjectId() { return projectId; }
+    public void setProjectId(int projectId) { this.projectId = projectId; }
+
+    @Override
+    public String toString() {
+        return "Task [id=" + id + ", title=" + title + ", status=" + status + "]";
+    }
+}
