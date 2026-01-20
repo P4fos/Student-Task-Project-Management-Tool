0ce1a700ef4b9a8d141e1d7ff3196e71d2e153aa
Alimzhan <kabylhanovalimzan@gmail.com>
2026-01-20 13:50:00 +0600

    Kabylkhnaov Alimzhan

--- src/edu/aitu/oop3/entities/User.java
+++ src/edu/aitu/oop3/entities/User.java
@@ -12,7 +12,7 @@ public class User {
         this.email = email;
     }
 
-    // Getters and Setters
+
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
     public String getUsername() { return username; }
