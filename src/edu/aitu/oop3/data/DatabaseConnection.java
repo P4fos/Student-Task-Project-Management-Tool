0086c8adeaa5385bc10b13a17627ae759228f13b
P4fos <110093231+P4fos@users.noreply.github.com>
2026-01-27 12:23:30 +0500

    Mailybay Zhalgas --> data base passport config

--- src/edu/aitu/oop3/data/DatabaseConnection.java
+++ src/edu/aitu/oop3/data/DatabaseConnection.java
@@ -2,26 +2,37 @@ package edu.aitu.oop3.data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DatabaseConnection implements IDB {
 
-
-    private final String URL = "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require";
-    private final String USER = "postgres.nbvwspufndatertzzlvr";
-    private final String PASSWORD = "UgXqW3PE986uBxrX";
-
-
-    public DatabaseConnection() {
-    }
-
     @Override
-    public Connection getConnection() throws SQLException, ClassNotFoundException {
+    public Connection getConnection() {
+        Properties props = new Properties();
+        String connectionUrl = "";
+        String user = "";
+        String pass = "";
+
+
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            props.load(fis);
+            connectionUrl = props.getProperty("db.url");
+            user = props.getProperty("db.user");
+            pass = props.getProperty("db.password");
+        } catch (IOException e) {
+            System.err.println("❌ ERROR: config.properties file not found in project root!");
+            e.printStackTrace();
+            return null;
+        }
+
         try {
             Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(connectionUrl, user, pass);
         } catch (Exception e) {
-            System.out.println("Connection Failed: " + e);
+            System.err.println("❌ CONNECTION ERROR: " + e.getMessage());
+            e.printStackTrace();
             return null;
         }
     }
