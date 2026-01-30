554cdddad2f92529bc0acf186676535e06cfa5e7
Alimzhan <kabylhanovalimzan@gmail.com>
2026-01-30 12:17:31 +0600

    Kabylkhanov Alimzhan --> Implement Builder pattern for Task entity

--- src/edu/aitu/oop3/entities/Task.java
+++ src/edu/aitu/oop3/entities/Task.java
@@ -1,27 +1,59 @@
 package edu.aitu.oop3.entities;
 
-import java.sql.Timestamp;
-
 public class Task {
     private int id;
     private String title;
     private String description;
-    private Timestamp deadline;
-    private String status;
-    private int projectId;
     private int userId;
-
-    public Task() {}
-
-
-    public Task(String title, String description, Timestamp deadline, int projectId, int userId) {
+    private String status;
+    public Task(int id, String title, String description, int userId, String status) {
+        this.id = id;
         this.title = title;
         this.description = description;
-        this.deadline = deadline;
-        this.status = "PENDING";
-        this.projectId = projectId;
         this.userId = userId;
+        this.status = status;
     }
+
+    private Task(Builder builder) {
+        this.id = builder.id;
+        this.title = builder.title;
+        this.description = builder.description;
+        this.userId = builder.userId;
+        this.status = "PENDING";
+    }
+
+
+    public static class Builder {
+        private int id;
+        private String title;
+        private String description;
+        private int userId;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setUserId(int userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Task build() {
+            return new Task(this);
+        }
+    }
+
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
@@ -32,17 +64,13 @@ public class Task {
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
 
-    public Timestamp getDeadline() { return deadline; }
-    public void setDeadline(Timestamp deadline) { this.deadline = deadline; }
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
 
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
 
-    public int getProjectId() { return projectId; }
-    public void setProjectId(int projectId) { this.projectId = projectId; }
 
-    public int getUserId() { return userId; }
-    public void setUserId(int userId) { this.userId = userId; }
     @Override
     public String toString() {
         return "Task [id=" + id + ", title=" + title + ", status=" + status + "]";
