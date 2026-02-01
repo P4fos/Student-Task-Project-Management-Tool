8c90e28bc1eeab453498821e1b12475be96b5f0f
P4fos <110093231+P4fos@users.noreply.github.com>
2026-02-01 11:38:47 +0500

    Mailybay Zhalgas --> fixed some minor mistakes

--- src/edu/aitu/oop3/data/DatabaseConnection.java
+++ src/edu/aitu/oop3/data/DatabaseConnection.java
@@ -3,12 +3,10 @@ package edu.aitu.oop3.data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
 public class DatabaseConnection implements IDB {
     private static DatabaseConnection instance;
-    private Connection connection;
 
     private DatabaseConnection() {}
 
@@ -22,25 +20,16 @@ public class DatabaseConnection implements IDB {
     @Override
     public Connection getConnection() {
         Properties props = new Properties();
-        String connectionUrl = "";
-        String user = "";
-        String pass = "";
-
         try (FileInputStream fis = new FileInputStream("config.properties")) {
             props.load(fis);
-            connectionUrl = props.getProperty("db.url");
-            user = props.getProperty("db.user");
-            pass = props.getProperty("db.password");
-        } catch (IOException e) {
-            System.err.println("ERROR: config.properties file not found!");
-            return null;
-        }
-
-        try {
             Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(connectionUrl, user, pass);
+            return DriverManager.getConnection(
+                    props.getProperty("db.url"),
+                    props.getProperty("db.user"),
+                    props.getProperty("db.password")
+            );
         } catch (Exception e) {
-            System.err.println(" CONNECTION ERROR: " + e.getMessage());
+            System.out.println("Connection Error: " + e.getMessage());
             return null;
         }
     }
--- src/edu/aitu/oop3/data/IDB.java
+++ src/edu/aitu/oop3/data/IDB.java
@@ -4,5 +4,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface IDB {
-    Connection getConnection() throws SQLException, ClassNotFoundException;
+
+    Connection getConnection();
 }
