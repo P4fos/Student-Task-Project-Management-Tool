9c65cd5bf630b24ab0a1172a77069cfebcad8e9e
Alimzhan <kabylhanovalimzan@gmail.com>
2026-02-03 00:48:26 +0600

    Kabylkhanov Alimzhan --> Added a new entity

---
+++ src/edu/aitu/oop3/entities/TaskFactory.java
@@ -0,0 +1,21 @@
+package edu.aitu.oop3.entities;
+
+public class TaskFactory {
+    public static Task createSimpleTask(int userId, String title, String description) {
+        return new Task.Builder()
+                .setId(0)
+                .setUserId(userId)
+                .setTitle(title)
+                .setDescription(description)
+                .build();
+    }
+
+    public static Task createUrgentTask(int userId, String title, String description) {
+        return new Task.Builder()
+                .setId(0)
+                .setUserId(userId)
+                .setTitle("URGENT: " + title)
+                .setDescription(description.toUpperCase() + " !!!")
+                .build();
+    }
+}
